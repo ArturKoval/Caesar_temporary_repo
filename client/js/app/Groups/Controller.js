69f953d0899b2788147fedd47908ef5987af0c1c
Yuriy Tatarincev <csnmetau@gmail.com>
2016-04-06 11:03:28 +0300

    groupListView added

--- client/js/app/Groups/Controller.js
+++ client/js/app/Groups/Controller.js
@@ -10,10 +10,13 @@
             /* var groupListView = new This.groupListView(); */ //later
 
             var contentView = new This.ContentView({model: new This.Group(app.store.groups[3])}),
-                groupView = new This.GroupView({model: new This.Group(app.store.groups[3])});
+                groupView = new This.GroupView({model: new This.Group(app.store.groups[3])}),
+                groupListView = new This.GroupListView({collection: new This.GroupList(app.store.groups)});
                 
             $('#content-header').append(contentView.renderHeader().$el);
 			$('#content-footer').append(contentView.renderFooter().$el);
+            $('#left-side-bar').append(groupListView.$el)
+                .append(groupListView.render());
 
             //this.collection = new This.GroupList(app.store.groups);
             //console.log(this.collection); 
