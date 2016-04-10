860031163f0e5e55f032f8d855eb20f6dca55da1
Yuriy Tatarincev <csnmetau@gmail.com>
2016-04-10 21:18:40 +0300

    createEditView validation uncomented ;)

--- client/js/app/Groups/view/GroupCreateEditView.js
+++ client/js/app/Groups/view/GroupCreateEditView.js
@@ -32,22 +32,22 @@
 
             this.model = model || new This.Group({teachers: [app.user.lastName]});
             //this.model.set(mock);
-        //    Backbone.Validation.bind(this, {
-        //        valid: function (view, attr, selector) {
-        //            var $el = view.$('[name=' + attr + ']'),
-        //                $group = $el.closest('.form-group');
-        //
-        //            $group.removeClass('has-error');
-        //            $group.find('.help-block').html('').addClass('hidden');
-        //        },
-        //        invalid: function (view, attr, error, selector) {
-        //            var $el = view.$('[name=' + attr + ']'),
-        //                $group = $el.closest('.form-group');
-        //
-        //            $group.addClass('has-error');
-        //            $group.find('.help-block').html(error).removeClass('hidden');
-        //        }
-        //    });
+            Backbone.Validation.bind(this, {
+                valid: function (view, attr, selector) {
+                    var $el = view.$('[name=' + attr + ']'),
+                        $group = $el.closest('.form-group');
+
+                    $group.removeClass('has-error');
+                    $group.find('.help-block').html('').addClass('hidden');
+                },
+                invalid: function (view, attr, error, selector) {
+                    var $el = view.$('[name=' + attr + ']'),
+                        $group = $el.closest('.form-group');
+
+                    $group.addClass('has-error');
+                    $group.find('.help-block').html(error).removeClass('hidden');
+                }
+            });
         },
 
         render: function () {
