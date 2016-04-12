1abd1d3d9e55b3dc000d84e7c532b0359069d394
Artem Zhylko <artem.zhylko@gmail.com>
2016-04-12 20:30:59 +0000

    Minor fix: GroupInfoVIew.js

--- client/js/app/Groups/view/GroupInfoView.js
+++ client/js/app/Groups/view/GroupInfoView.js
@@ -13,7 +13,7 @@
 
         render: function () {
             this.$el.html(this.template(this.model.toJSON()));
-            console.log(this.model.toJSON());
+
             return this;
         }
     });
