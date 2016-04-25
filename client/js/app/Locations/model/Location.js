7a6c6308ad0529dc5ef39373db7c5dfd20444d7d
poznukhovd <poznukhovd@gmail.com>
2016-04-25 12:10:29 +0300

    Locations: Location model update

--- client/js/app/Locations/model/Location.js
+++ client/js/app/Locations/model/Location.js
@@ -14,6 +14,26 @@
             };
         },
 
+        sync: function(method, model, options) {
+            var request = '';
+
+            if (method === 'update' || method === 'create') {
+                model.unset('isChecked', {silent: true});
+
+                request = Backbone.sync.call(model, method, model, options);
+            } else {
+                request = Backbone.sync.call(this, method, this, options);
+            }
+
+            return request;
+        },
+
+        parse: function (data) {
+            data.isChecked = false;
+
+            return data;
+        },
+
         toggleCheck: function () {
             this.set('isChecked', !this.get('isChecked'));
         },
