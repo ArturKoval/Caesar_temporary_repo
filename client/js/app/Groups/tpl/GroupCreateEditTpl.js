39ad8e4dd82e1cdc3361adaa00a31a358675b4c8
Hex <Sergeeva.Anastacia@gmail.com>
2016-04-26 01:26:37 +0300

    Groups: createEdit view logic for group name autogeneration implemented; return option for old version of group name implemented

--- client/js/app/Groups/tpl/GroupCreateEditTpl.js
+++ client/js/app/Groups/tpl/GroupCreateEditTpl.js
@@ -7,7 +7,7 @@ templates.groupEditCreate = _.template([
 '                                <div class="form-group name-wrapper col-xs-12">',
 '                                    <label class="control-label">Group name</label>',
 '                                    <input type="text" class="form-control" name="name" value="<%= name %>" tabindex="1">',
-'                                    <span class="return-name"></span>',
+'                                    <span class="return-name" hidden></span>',
 '                                 </div>',
 '                                   <% if (!isCreate) { %>', 
 '                                   <div class="form-group col-xs-12 stage-wrapper">',
