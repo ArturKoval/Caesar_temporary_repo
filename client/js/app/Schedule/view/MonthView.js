6c273f65d3f14e6d99875823f5a5f83c9328a797
Yuriy Tatarincev <csnmetau@gmail.com>
2016-04-26 02:07:22 +0300

    monthView implemented, css for monthView implemented

---
+++ client/js/app/Schedule/view/MonthView.js
@@ -0,0 +1,87 @@
+//start this module with next code;
+//var monthView = new CS.Schedule.MonthView();
+//$('#main-section').html(monthView.render(new Date().getFullYear(), new Date().getMonth()).el);
+
+(function (This, app) {
+    This.MonthView = Backbone.View.extend({
+
+        className: 'calendarView',
+        months: ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"],
+
+        events: {
+            'click .prevMonth': function () {this.toggleMonth('prev')},
+            'click .nextMonth': function () {this.toggleMonth('next')}
+        },
+
+        shownMonth: new Date().getMonth(),
+        shownYear: new Date().getFullYear(),
+
+        render: function (year, month) {
+            this.$el.html(templates.calendar({month: this.months[month]}));
+            this.calendar = this.$el.find('.calendar');
+
+            var date = new Date(year, month),
+                table = templates.dayNames(),
+                weekNum = 1,
+                i, j;
+
+            for (i = 0; i < getDay(date); i++) {
+                table += '<td></td>';
+            }
+
+            while (date.getMonth() === month) {
+                table += templates.day({day: date.getDate()});
+
+                if (getDay(date) % 7 == 6) {
+                    table += templates.weekRow({weekNum: weekNum++})
+                }
+
+                date.setDate(date.getDate() + 1);
+            }
+
+            if (getDay(date) !== 0) {
+                for (j = getDay(date); j < 7; j++) {
+                    table += '<td></td>';
+                }
+            }
+
+            table += '</tr></table>';
+
+            function getDay(date) { //returns monday first, not Sunday as usual;
+                var day = date.getDay();
+                if (day === 0) {
+                    day = 7
+                }
+                return day - 1;
+            }
+
+            this.calendar.html(table);
+            $('.stageView').html(this.shownYear); // stub
+            return this;
+        },
+
+        toggleMonth: function(month) {
+            if (month === 'prev') {
+            this.shownMonth -=  1;
+            } else if (month === 'next') {
+                this.shownMonth += 1;
+            }
+
+           if (this.shownMonth === -1) {
+               this.shownMonth = 11;
+               this.shownYear -= 1;
+           }
+
+            if (this.shownMonth === 12) {
+                this.shownMonth = 0;
+                this.shownYear += 1;
+            }
+
+            this.render(this.shownYear,  this.shownMonth)
+
+        }
+    });
+})(CS.Schedule, app);
+
+
