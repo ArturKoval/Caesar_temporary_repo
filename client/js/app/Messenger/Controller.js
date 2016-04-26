fa3397a81c47765cb64c51c193e4bf742270bddc
poznukhovd <poznukhovd@gmail.com>
2016-04-26 12:10:06 +0300

    Hint messages fixed

--- client/js/app/Messenger/Controller.js
+++ client/js/app/Messenger/Controller.js
@@ -9,26 +9,39 @@
         initialize: function () {
             this.mediator = app.mediator;
             this.messageRouter = {
-                'confirmation': {view: 'ConfirmationView', el: '#modal-window'},
-                'flash-info': {view: 'FlashMessageView', el: '.flashMessage'},
-                'flash-warning': {view: 'FlashMessageView', el: '.flashMessage'}
-           };
+                'confirmation': {
+                    view: 'ConfirmationView',
+                    el: '#modal-window'
+                },
+                'flash-info': {
+                    view: 'FlashMessageView',
+                    el: '.flashMessage'
+                },
+                'flash-warning': {
+                    view: 'FlashMessageView',
+                    el: '.flashMessage'
+                }
+            };
         },
 
         showMessage: function (data) {
             var m, hintName, view, hintView;
-            
+
             if (data.type === 'hints') {
                 data.hints.forEach(function (hint) {
-                    hintName = '#' + hint.name,
-                    m = new This.Messenger(hint),
-                    hintView = new This.HintView({model: m}).render().$el;
-                    data.$el.find(hintName).before(hintView);
-                }); 
+                    hintName = hint.name;
+                    m = new This.Messenger(hint);
+                    hintView = new This.HintView({
+                        model: m
+                    }).render().$el;
+                    data.$el.find('[name="' + hintName + '"]').before(hintView);
+                });
             } else {
-                m = new This.Messenger(data),
-                view = new This[this.messageRouter[data.type].view]({model: m});
-                $(''+ this.messageRouter[data.type].el).append(view.render().$el);
+                m = new This.Messenger(data);
+                view = new This[this.messageRouter[data.type].view]({
+                    model: m
+                });
+                $('' + this.messageRouter[data.type].el).append(view.render().$el);
             }
         }
     });
