b2d3b418b7576a2470d9567fef65db79d359174b
ArturSS <arutrkovalua@gmail.com>
2016-08-16 16:07:11 +0300

    merge with Artur_Dmitriy_Kirille

--- client/js/app/Students/model/Student.js
+++ client/js/app/Students/model/Student.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function (This) {
-	This.Student = Backbone.Model.Extend({
+	This.Student = Backbone.Model.extend({
 		defaults: {
 			groupId: '',
 			name: '',
--- client/js/app/Students/model/Students.js
+++ client/js/app/Students/model/Students.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function (This) {
-	This.Students = Backbone.Collection.Extend({
+	This.Students = Backbone.Collection.extend({
 		model: This.Student,
 
 		url: '/students'
