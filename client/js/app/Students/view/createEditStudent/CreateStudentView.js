3efdf2b13100f95231f4e03ffd54d9c0c0e564e1
Petr Kucher <supershmell@gmail.com>
2016-08-23 04:18:12 +0300

    Sorting fixed

--- client/js/app/Students/view/createEditStudent/CreateStudentView.js
+++ client/js/app/Students/view/createEditStudent/CreateStudentView.js
@@ -10,7 +10,9 @@
 
         events: {
             'click .close-modal-window': 'exit',
-            'click .save-changes': 'createNewStudent'
+            'click .save-changes': 'createNewStudent',
+            'click th': 'tableSort'
+
         },
 
         initialize: function () {
@@ -114,6 +116,108 @@
                 score = Math.round(parseFloat(score)*10)/10;
             }
             return result;
+        },
+
+        tableSort: function (e) {
+            var $grid = document.querySelector('.students_list');
+
+            sortGrid(e.target.cellIndex, e.target, e.target.id);
+
+            function sortGrid (colNum, element, idName) {
+                var tbody = $('tbody')[0],
+                    rowsArray = [].slice.call(tbody.rows), //make an Array
+                    compare; //comparing function
+
+                if (colNum === 0) {
+                    if (idName==='sortUp') {
+                        element.removeAttribute('sortUp');
+                        element.setAttribute( 'id','sortDown');
+                        
+                        compare = function (rowA, rowB) {   
+                        return rowB.cells[colNum].innerHTML > rowA.cells[colNum].innerHTML ? 1 : -1;
+                        }
+    
+                    } else if (idName==='sortDown') {
+                        element.removeAttribute('sortDown');
+                        element.setAttribute('id','sortUp');
+                        
+                        compare = function (rowA, rowB) {   
+                        return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1 : -1;
+                        }
+                    
+                    } else {
+                        element.setAttribute('id','sortUp');
+                            
+                        compare = function (rowA, rowB) {   
+                        return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1 : -1;
+                        }
+                    }
+                }
+    
+                if (colNum === 2) {
+                    addClassRange(rowsArray);
+
+                    if (idName==='sortUp') {
+                        element.removeAttribute('sortUp');
+                        element.setAttribute( 'id','sortDown');
+                        
+                        compare = function (rowA, rowB) {   
+                        return rowA.cells[colNum].className > rowB.cells[colNum].className ? 1 : -1;
+                        }
+    
+                    } else if (idName==='sortDown') {
+                        element.removeAttribute('sortDown');
+                        element.setAttribute('id','sortUp');
+                        
+                        compare = function (rowA, rowB) {   
+                        return rowB.cells[colNum].className > rowA.cells[colNum].className ? 1 : -1;
+                        }
+                    
+                    } else {
+                        element.setAttribute('id','sortUp');
+        
+                        compare = function (rowA, rowB) {   
+                        return rowB.cells[colNum].className > rowA.cells[colNum].className ? 1 : -1;
+                        }
+                    }
+
+                }
+                
+            function addClassRange (rowsArray) {
+                rowsArray.forEach(function(row) {
+                    var value = row.cells[colNum].innerHTML,
+                        valueClass = row.cells[colNum].classList,
+                        classNumbers;
+
+                    classNumbers = {
+                        'Elementary':'0',
+                        'Pre-intermediate low':'1',
+                        'Pre-intermediate':'2',
+                        'Pre-intermediate strong':'3',
+                        'Intermediate low':'4',
+                        'Intermediate':'5',
+                        'Intermediate strong':'6',
+                        'Upper-intermediate low':'7',
+                        'Upper-intermediate':'8',
+                        'Upper-intermediate strong':'9',
+                        'Advanced':'a',
+                        }
+
+                    valueClass.add(classNumbers[value]);
+                });
+            }
+
+            rowsArray.sort(compare); 
+
+            $grid.removeChild(tbody);
+
+            for (var i = 0; i < rowsArray.length; i++) {
+                tbody.appendChild(rowsArray[i]);
+            }
+
+            $grid.appendChild(tbody);
+            }
+
         },
 
         exit: function () {
